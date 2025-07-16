20a5bbc0d1a440bdddd76311b53adbf62ee19c78
ChandlerMurphy <chandlermurphy02@gmail.com>
2025-07-16 17:08:45 -0600

    Redid middleware and changes to see if Vercel will now deploy

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -5,9 +5,8 @@ import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
-
  
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
  
 async function getUser(email: string): Promise<User | undefined> {
   try {
